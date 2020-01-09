93b23d69b330d992bb3692c5e690aa456726161c
David Bond <david.bond@panoramicdata.com>
2020-01-09 17:55:50 +0000

    Initial commit

---
+++ PanoramicData.ConsoleExtensions/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace PanoramicData.ConsoleExtensions
+{
+	public static class ConsolePlus
+	{
+		public static string ReadPassword()
+		{
+			var result = new StringBuilder();
+			while (true)
+			{
+				ConsoleKeyInfo key = Console.ReadKey(true);
+				switch (key.Key)
+				{
+					case ConsoleKey.Enter:
+						return result.ToString();
+					case ConsoleKey.Backspace:
+						if (result.Length == 0)
+						{
+							continue;
+						}
+						result.Length--;
+						Console.Write("\b \b");
+						continue;
+					default:
+						result.Append(key.KeyChar);
+						Console.Write("*");
+						continue;
+				}
+			}
+		}
+	}
+}
