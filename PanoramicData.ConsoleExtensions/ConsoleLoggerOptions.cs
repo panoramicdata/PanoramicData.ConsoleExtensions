05d691b211cd6cfd0585913dd5d941f548e77bcf
Ecstatik <johankool2003@hotmail.co.uk>
2023-06-13 11:21:49 +0100

    Codacy Fix

--- PanoramicData.ConsoleExtensions/ConsoleLoggerOptions.cs
+++ PanoramicData.ConsoleExtensions/ConsoleLoggerOptions.cs
@@ -3,15 +3,16 @@
 namespace PanoramicData.ConsoleExtensions
 {
 	public class ConsoleLoggerOptions
-	{
-		public ConsoleColor TraceColor = ConsoleColor.White;
-		public ConsoleColor DebugColor = ConsoleColor.Cyan;
-		public ConsoleColor InformationColor = ConsoleColor.Gray;
-		public ConsoleColor WarningColor = ConsoleColor.Yellow;
-		public ConsoleColor ErrorColor = ConsoleColor.Red;
-		public ConsoleColor CriticalColor = ConsoleColor.Magenta;
-		public ConsoleColor NoneColor = ConsoleColor.DarkGray;
-		public LogLevel LogLevel = LogLevel.Information;
-		public string? TimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
-	}
+{
+	public ConsoleColor TraceColor {get;} = ConsoleColor.White;
+	public ConsoleColor DebugColor {get;} = ConsoleColor.Cyan;
+	public ConsoleColor InformationColor {get;} = ConsoleColor.Gray;
+	public ConsoleColor WarningColor {get;} = ConsoleColor.Yellow;
+	public ConsoleColor ErrorColor {get;} = ConsoleColor.Red;
+	public ConsoleColor CriticalColor {get;} = ConsoleColor.Magenta;
+	public ConsoleColor NoneColor {get;} = ConsoleColor.DarkGray;
+	public LogLevel LogLevel {get;} = LogLevel.Information;
+	public string TimeFormat {get;} = "yyyy-MM-dd HH:mm:ss.fff";
+}
+
 }
